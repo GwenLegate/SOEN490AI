ce7f47c87157acdacc205c388403613e7d66d7c5
GwenLegate <gwendolyne_l@outlook.com>
2020-10-17 23:33:19 -0400

    added plot function for logfile

--- utilities/data_processing.py
+++ utilities/data_processing.py
@@ -2,6 +2,8 @@
 
 # Import statements
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # returns a usable format of the input data.  Input (str): path to fil to be loaded, type (str): type of file input
 # currently only accepts *.csv, can be extended to other data formats if required later in development
@@ -22,3 +24,38 @@ def check_balance(dict, labels):
         dict[int(y)] += 1
         total += 1
     print(dict)
+
+'''creates plots of loss training and loss testing and accuracy training and accuracy testing over time'''
+def plot_accuracy_and_loss(logfile_path):
+    data = pd.read_csv(logfile_path).to_numpy()
+    times = data[:, -6]
+    epoch = data[:, -6:-4]
+    train_accuracy = data[:, -4]
+    train_loss = data[:, -3]
+    test_accuracy = data[:, -2]
+    test_loss = data[:, -1]
+    fig, axs = plt.subplots(2)
+
+    '''plot epochs as vertical lines'''
+    filter_epoch = np.array(epoch[0, :])
+    last = epoch[0, 1]
+    for e in epoch:
+        if last != e[1]:
+            np.vstack((filter_epoch, e))
+            last = e[1]
+
+    axs[0].plot(times, test_accuracy, label="train accuracy")
+    axs[0].plot(times, train_accuracy, label="test accuracy")
+    axs[0].set_title("Accuracy")
+    axs[0].set(xlabel="time", ylabel="Accuracy")
+    axs[0].vlines(filter_epoch[0], 0, 1, linestyles="dotted", label="epochs")
+    axs[0].legend()
+
+    axs[1].plot(times, test_loss, label="train loss")
+    axs[1].plot(times, train_loss, label="test loss")
+    axs[1].set_title("Loss")
+    axs[1].set(xlabel="time", ylabel="Loss")
+    axs[0].vlines(filter_epoch[0], 0, 1, linestyles="dotted", label="epochs")
+    axs[1].legend()
+
+    plt.show()
