3d24dc0d5a9756d68c418a592308521cce80abeb
GwenLegate <gwendolyne_l@outlook.com>
2020-11-05 02:36:05 -0500

    changed the model configuration so that the image inputs are size invariant and to try to improve the quality of the predictions.  Added real images to test output

--- alphabet_model/test_alphabet_model.py
+++ alphabet_model/test_alphabet_model.py
@@ -1,4 +1,6 @@
 import torch
+import PIL
+import numpy as np
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -39,4 +41,24 @@ def predict_test_set():
 '''generate plot of accuracy and loss over time'''
 if PLOT_ACCURACY_LOSS:
     data.plot_accuracy_and_loss(os.getcwd()+"/alphabet_model.log")
+
+'''real image test'''
+
+alphabet_cnn = Net()
+alphabet_cnn.load_state_dict(torch.load(c.MODEL_SAVE_PATH + "/alphabet_model.pt", map_location=device))
+
+alpha_key = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H", 8:"I", 9:"J", 10:"K", 11:"L", 12:"M", 13:"N", 14:"O", 15:"P", 16:"Q", 17:"R", 18:"S", 19:"T", 20:"U", 21:"V", 22:"W", 23:"X", 24:"Y", 25:"Z"}
+
+img = PIL.Image.open(c.GWEN_W).convert("L")
+web_l = np.array(img) / 255.0
+w, l = web_l.shape
+
+def predict_az(input):
+    input_tensor = torch.from_numpy(input).view(-1, w, l).type('torch.FloatTensor')
+    predict_vect = alphabet_cnn(input_tensor.view(-1, 1, w, l)).detach().numpy()
+    predict_val = np.argmax(predict_vect)
+    return alpha_key[predict_val]
+
+print(predict_az(web_l))
+
 
--- constants.py
+++ constants.py
@@ -9,4 +9,25 @@ FILE_DIGIT_LABELS = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
                       "/SOEN490AI/datasets/asl_digits/Y.npy"
 MODEL_SAVE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
                      "/SOEN490AI/trained_models"
-
+GWEN_B = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_b.png"
+GWEN_H = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_h2828.png"
+GWEN_Y = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_y.jpg"
+GWEN_W = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_w.png"
+GWEN_C = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_c2828.png"
+WEB_L = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/web_l.png"
+WEB_A = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/web_a2828.png"
+GWEN_R = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_r.png"
+GWEN_F = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_f.png"
+GWEN_E = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_e.png"
+GWEN_X = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+                     "/SOEN490AI/datasets/asl_alphabet/gwen_x.png"
