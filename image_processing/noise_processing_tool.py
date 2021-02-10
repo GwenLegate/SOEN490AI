53a3f98b7c01a9ed540a2a8a0d10d83d898e2b7c
Angel Chung <achung998@gmail.com>
2021-02-10 04:20:44 -0500

    Added utility to add noise for images

---
+++ image_processing/noise_processing_tool.py
@@ -0,0 +1,43 @@
+import time
+from random import seed
+from random import randint
+import cv2
+from skimage.util import random_noise
+
+GAUSSIAN = "gaussian"
+SALT_PEPPER = "s&p"
+POISSON = "poisson"
+
+def generate_random_noise(img, mode, seed):
+    return random_noise(img, mode=mode, seed=seed)
+
+# ==================================== MAIN ====================================
+'''
+    Applies gaussian, salt and pepper or poisson noise randomly to a given picture. Will randomly apply one of the noises randomly using the time as seed.
+    val_seed is the random seed to have randomness to the noise application.
+
+    Return noisy image as float values (Float values from 0 to 1)
+'''
+def apply_noise(img_path):
+    seed(time.time()) # For generating random numbers based on system time
+    val = randint(0, 2)
+    val_seed = randint(0, 100) # For pseudo randomness in the noise application
+
+    print(val)
+    print(val_seed)
+
+    img = cv2.imread(img_path, 0)
+
+    '''
+        0: gaussian noise
+        1: salt and pepper noise
+        2: poisson
+    '''
+    if val == 0:
+        noisy_image = generate_random_noise(img, GAUSSIAN, val_seed)
+    elif val == 1:
+        noisy_image = generate_random_noise(img, SALT_PEPPER, val_seed)
+    else:
+        noisy_image = generate_random_noise(img, POISSON, val_seed)
+
+    return noisy_image
